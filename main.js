7e15b236ffdc1de505af3b2ea9ee1a321d2eabc3
Tim <timhuang814@gmail.com>
2021-06-16 19:25:17 -0400

    updated with comments and new spam coomand

--- main.js
+++ main.js
@@ -10,21 +10,46 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', async message => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
+client.on('interaction', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
 
-    if (message.content === commandSymbol.concat('anime')) {
+client.on('message', async message => {
+    //ping command
+    if (message.content.toLowerCase() === 'ping') {
+      console.log(message.content)
+      message.channel.send('Pong!');
+    }
+    //anime picture command
+    if (message.content.toLowerCase() === commandSymbol.concat('anime')) {
+      console.log(message.content)
       const anime = randomanime.anime();
       message.channel.send(anime);
     }
-
-    if (message.content === commandSymbol.concat('animensfw')) {
+    //anime nsfw picture command
+    if (message.content.toLowerCase() === commandSymbol.concat('animensfw')) {
+      console.log(message.content)
       const anime = randomanime.nsfw();
       message.channel.send(anime);
     }
 
+    if (message.content.toLowerCase() === commandSymbol.concat('spam')) {
+      console.log(message.content)
+      spamTest(message);
+    }
+
   });
+
+  async function spamTest(message){
+    for(i = 0; i < 21; i++){
+      message.channel.send('Spam!');
+    }
+    // while(true){
+    //   message.channel.send('Spam!');
+    // }
+  }
 
 client.login(process.env.TOKEN);
