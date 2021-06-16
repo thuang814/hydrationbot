484610731bfc6a108b82ba873822fd52b644fc85
Tim <timhuang814@gmail.com>
2021-06-16 18:28:13 -0400

    added basic commands
    ping command + anime command

--- main.js
+++ main.js
@@ -2,15 +2,29 @@ require('dotenv').config()
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const randomanime = require('random-anime')
+
+const commandSymbol='$'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
+client.on("message", async message => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+
+    if (message.content === commandSymbol.concat("anime")) {
+      const anime = randomanime.anime();
+      message.channel.send(anime);
+    }
+
+    if (message.content === commandSymbol.concat("animensfw")) {
+      const anime = randomanime.nsfw();
+      message.channel.send(anime);
+    }
+
+  });
 
 client.login(process.env.TOKEN);
